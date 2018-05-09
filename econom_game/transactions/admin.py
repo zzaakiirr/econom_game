be8d1d93d4e7b065e6a90533dcf51a991d6f3d8f
zzaakiirr <zzaakiirr@gmail.com>
2018-05-09 20:37:52 +0300

    Added permissions for superuser
    Can add new Teams, Stations, Transactions

--- econom_game/transactions/admin.py
+++ econom_game/transactions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from stations.models import Station
+from teams.models import Team
+from transactions.models import Transaction
 
-# Register your models here.
+
+admin.site.register(Station)
+admin.site.register(Team)
+admin.site.register(Transaction)
