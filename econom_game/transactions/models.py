abffa8084557105ff2b52385005f34264aca8a18
zzaakiirr <zzaakiirr@gmail.com>
2018-05-09 21:23:59 +0300

    Added sender to Transaction model (used GenericForeignKey)

--- econom_game/transactions/models.py
+++ econom_game/transactions/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Transaction(models.Model):
     id = models.PositiveIntegerField(primary_key=True, unique=True)
 
-    # TO FIX: Use GenericForeignKey
-    # sender =
-    # recipient =
+    sender_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        default=None)
+    sender_id = models.PositiveIntegerField(default=None)
+    sender = GenericForeignKey('sender_type', 'sender_id')
 
     amount = models.PositiveIntegerField()
     datetime = models.DateTimeField(auto_now_add=True)
