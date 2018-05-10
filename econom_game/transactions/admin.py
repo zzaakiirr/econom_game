8fd96ef5e532c3346daca8b87593794e7bcf93aa
zzaakiirr <zzaakiirr@gmail.com>
2018-05-10 18:34:20 +0300

    Added permission to change Cards of Team for superuser

--- econom_game/transactions/admin.py
+++ econom_game/transactions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from stations.models import Station
-from teams.models import Team
+from teams.models import Team, Card
 from transactions.models import Transaction
 
 
 admin.site.register(Station)
 admin.site.register(Team)
+admin.site.register(Card)
 admin.site.register(Transaction)
