50e0c04b481383e0d0aa0122eca533ad51734110
zzaakiirr <zzaakiirr@gmail.com>
2018-05-12 13:46:39 +0300

    Refactored tests for teams view

--- econom_game/teams/tests.py
+++ econom_game/teams/tests.py
@@ -2,9 +2,9 @@ from django.urls import reverse, resolve
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
 
-from teams.models import Team, Card
-from teams.serializers import TeamSerializer
-from teams.views import ListTeamsView
+from .models import Team, Card
+from .serializers import TeamSerializer
+from .views import ListTeamsView
 
 
 class BaseViewTest(APITestCase):
@@ -14,21 +14,20 @@ class BaseViewTest(APITestCase):
         card = Card.objects.create(id=1000, cvv=999, money_amount=999)
         Team.objects.create(id=1000, name="team_1", login="team_1", card=card)
 
+        url = reverse("teams-all")
+        self.response = self.client.get(url)
+
 
 class GetAllTeamsTest(BaseViewTest):
 
-    def test_get_all_teams(self):
-        """
-        This test ensures that all teams added in the setUp method
-        exist when we make a GET request to the api/teams/ endpoint
-        """
-        response = self.client.get(reverse("teams-all"))
-        expected = Team.objects.all()
-        serialized = TeamSerializer(expected, many=True)
+    def test_get_all_teams_view_success_status_code(self):
+        self.assertEquals(self.response.status_code, status.HTTP_200_OK)
 
-        self.assertEqual(response.data, serialized.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_view_function(self):
+    def test_get_all_teams_url_resolves_get_all_teams_view(self):
         view = resolve('/api/teams/')
         self.assertEquals(view.func.view_class, ListTeamsView)
+
+    def test_get_all_teams_return_correct_data(self):
+        expected = Team.objects.all()
+        serialized = TeamSerializer(expected, many=True)
+        self.assertEquals(self.response.data, serialized.data)
