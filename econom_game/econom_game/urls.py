af20c0731068903522a964557274ecb92a6864b2
zzaakiirr <zzaakiirr@gmail.com>
2018-05-12 13:21:22 +0300

    Added url api/teams/ for getting all teams response

--- econom_game/econom_game/urls.py
+++ econom_game/econom_game/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from teams.views import ListTeamsView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/teams/$', ListTeamsView.as_view(), name='teams-all'),
 ]
