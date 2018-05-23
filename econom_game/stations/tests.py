90e9cf77cc6c393aeea9ea989ee9613b44b9e628
zzaakiirr <zzaakiirr@gmail.com>
2018-05-23 10:47:01 +0300

    Refactored stations tests with setUp() method

--- econom_game/stations/tests.py
+++ econom_game/stations/tests.py
@@ -1,6 +1,8 @@
 from django.urls import reverse, resolve
 from django.test import TestCase, Client
 
+from teams.tests import SuperUserTestCase
+
 from .models import Station
 from .serializers import StationSerializer
 from .views import ListStationsView, create_station
@@ -9,7 +11,7 @@ from .views import ListStationsView, create_station
 class BaseViewTest(TestCase):
     def setUp(self):
         Station.objects.create(
-            id=1000, name="station_1",
+            id=999, name="station_999",
             complexity=2, min_bet=100, max_bet=200,
         )
         url = reverse("all_stations")
@@ -30,14 +32,14 @@ class GetAllStationsTest(BaseViewTest):
         self.assertEquals(self.response.data, serialized.data)
 
 
-class CreateStationTest(TestCase):
-    client = Client()
-    client.login(username='admin', password='password123')
-    url = (
-        "%s?id=999&name=station&complexity=2&min_bet=0&max_bet=0" %
-        reverse("create_station")
-    )
-    response = client.get(url)
+class CreateStationTest(SuperUserTestCase):
+    def setUp(self):
+        super().setUp()
+        url = (
+            "%s?id=999&cvv=999&money_amount=999" %
+            reverse("create_card")
+        )
+        self.response = self.client.get(url)
 
     def test_create_team_view_success_status_code(self):
         self.assertEquals(self.response.status_code, 200)
@@ -45,6 +47,3 @@ class CreateStationTest(TestCase):
     def test_create_station_url_resolves_create_station_view(self):
         view = resolve('/api/m=create_station/')
         self.assertEquals(view.func, create_station)
-
-    station_created_for_test = Station.objects.get(id=999)
-    station_created_for_test.delete()
