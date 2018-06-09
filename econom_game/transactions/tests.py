c5d44906c84277b77d0b91feeb103707e1f6446d
zzaakiirr <zzaakiirr@gmail.com>
2018-06-09 18:44:27 +0300

    Renamed module name & its imports in transaction app

--- econom_game/transactions/tests.py
+++ econom_game/transactions/tests.py
@@ -7,7 +7,7 @@ from transactions.models import Transaction
 
 from transactions.views import make_transaction
 
-from transactions.transactions_test_helpers import make_transaction_request_url
+from transactions.test_helpers import make_transaction_request_url
 
 
 class MakeTransasctionTests(TestCase):
