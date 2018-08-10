e1ff97ce23074e3723c1c7750bd25cf278ab345f
zzaakiirr <zzaakiirr@gmail.com>
2018-08-11 02:07:53 +0300

    Trying another option on getting json file (login page)

--- econom_game/accounts/views.py
+++ econom_game/accounts/views.py
@@ -8,10 +8,9 @@ from django.contrib.auth import authenticate, login
 @csrf_exempt
 def login_user(request):
     if request.method == 'POST':
-        data = request.body.decode('utf-8')
-        received_json_data = json.loads(data)
-        username = received_json_data[username]
-        password = received_json_data[password]
+        data = request.POST.dict()
+        username = data[username]
+        password = data[password]
         user = authenticate(username=username, password=password)
         if user is not None:
             if login(request, user):
