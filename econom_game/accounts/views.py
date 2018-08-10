87913166253f8f85ea749d55cc250ad182ac7087
zzaakiirr <zzaakiirr@gmail.com>
2018-08-11 01:44:01 +0300

    Deleted rendering form in login page & added json manipulation

--- econom_game/accounts/views.py
+++ econom_game/accounts/views.py
@@ -1,24 +1,19 @@
+import json
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth import authenticate, login
 
-from .forms import LoginForm
-
 
 @csrf_exempt
 def login_user(request):
     if request.method == 'POST':
-        form = LoginForm(request.POST)
-        username = request.POST['username']
-        password = request.POST['password']
+        data = json.loads(request.body.decode("utf-8"))
+        username = data[username]
+        password = data[password]
         user = authenticate(username=username, password=password)
         if user is not None:
-            login(request, user)
-            return JsonResponse({"status": "success"})
-        else:
-            return JsonResponse(
-                {"status": "failure"})
-    else:
-        form = LoginForm()
-        return render(request, 'login.html', {'form': form})
+            if login(request, user):
+                return JsonResponse({"status": "success"})
+
+    return JsonResponse({"status": "failure"})
