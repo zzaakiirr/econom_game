6780598638ad925e409f4983f5f19da84c6ebdad
zzaakiirr <zzaakiirr@gmail.com>
2018-08-11 14:39:17 +0300

    Fixed login page with json data

--- econom_game/accounts/views.py
+++ econom_game/accounts/views.py
@@ -1,19 +1,18 @@
 import json
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
 from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
 
 
 @csrf_exempt
 def login_user(request):
     if request.method == 'POST':
         data = json.loads(request.body.decode("utf-8"))
-        username = data[username]
-        password = data[password]
+        username = data['username']
+        password = data['password']
         user = authenticate(username=username, password=password)
         if user is not None:
-            if login(request, user):
-                return JsonResponse({"status": "success"})
+            login(request, user)
+            return JsonResponse({"status": "success"})
 
     return JsonResponse({"status": "failure"})
