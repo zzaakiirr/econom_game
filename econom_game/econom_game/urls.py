305e2305d4cae5d56602095b2f267ebd31511188
zzaakiirr <zzaakiirr@gmail.com>
2018-08-12 19:11:41 +0300

    Added /station url which returns empty page

--- econom_game/econom_game/urls.py
+++ econom_game/econom_game/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from .empty_view import empty_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,4 +10,6 @@ urlpatterns = [
     url(r'', include('teams.urls')),
     url(r'', include('stations.urls')),
     url(r'', include('transactions.urls')),
+
+    url(r'^station/$', empty_view, name="station"),
 ]
