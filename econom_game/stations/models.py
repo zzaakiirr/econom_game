df6a77d02c10b08bc5dec39481c8d051ed49214d
zzaakiirr <zzaakiirr@gmail.com>
2018-08-12 19:15:34 +0300

    Added permissions for Station model & migrated it

--- econom_game/stations/models.py
+++ econom_game/stations/models.py
@@ -18,5 +18,10 @@ class Station(models.Model):
     min_bet = models.PositiveIntegerField()
     max_bet = models.PositiveIntegerField()
 
+    class Meta:
+        permissions = (
+            ('station', 'Can view "/station" page'),
+        )
+
     def __str__(self):
         return self.name
