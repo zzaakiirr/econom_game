fe80fc5e7af0a2f70e3fae493c267fa32aba3de1
zzaakiirr <zzaakiirr@gmail.com>
2018-08-12 19:17:29 +0300

    Added 'get_menu' view which returns list of pages which user can visit

--- econom_game/accounts/views.py
+++ econom_game/accounts/views.py
@@ -1,7 +1,9 @@
-import json
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+
+from .views_helpers import get_user_allowed_urls
 
 
 @csrf_exempt
@@ -13,6 +15,14 @@ def login_user(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return JsonResponse({"status": "success"})
+            return JsonResponse({"success": True})
 
-    return JsonResponse({"status": "failure"})
+    return JsonResponse({"success": False})
+
+
+@login_required
+def get_menu(request):
+    user_allowed_urls = get_user_allowed_urls(request)
+    return JsonResponse(
+        {"success": True, "user_allowed_urls": user_allowed_urls}
+    )
--- econom_game/econom_game/urls.py
+++ econom_game/econom_game/urls.py
@@ -6,6 +6,7 @@ from .empty_view import empty_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/v1/get_menu', accounts_views.get_menu, name="get_menu"),
     url(r'', include('accounts.urls')),
     url(r'', include('teams.urls')),
     url(r'', include('stations.urls')),
