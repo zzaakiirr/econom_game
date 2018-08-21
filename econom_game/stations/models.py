2c4c9aa4a3b5bff7bebea2067b7f209f7b8cd4a2
zzaakiirr <zzaakiirr@gmail.com>
2018-08-21 18:56:58 +0300

    Changed Station model's complexity field type

--- econom_game/stations/models.py
+++ econom_game/stations/models.py
@@ -5,17 +5,7 @@ class Station(models.Model):
     id = models.PositiveIntegerField(primary_key=True, unique=True)
     name = models.CharField(max_length=30)
     owner = models.CharField(max_length=30)
-
-    LOW = 2
-    NORMAL = 2.5
-    HIGH = 3
-    COMPLEXITY_CHOICES = (
-        (LOW, 'Low'),
-        (NORMAL, 'Normal'),
-        (HIGH, 'High'),
-    )
-
-    complexity = models.PositiveIntegerField(choices=COMPLEXITY_CHOICES)
+    complexity = models.FloatField()
     min_bet = models.PositiveIntegerField()
     max_bet = models.PositiveIntegerField()
 
