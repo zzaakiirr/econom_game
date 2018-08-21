5259f950d9d7ca3ce78d09b6fc329306a22214fd
zzaakiirr <zzaakiirr@gmail.com>
2018-08-21 21:01:37 +0300

    Switched error messages to russian

--- econom_game/stations/views.py
+++ econom_game/stations/views.py
@@ -18,7 +18,7 @@ class ListStationsView(generics.ListAPIView):
 @csrf_exempt
 def create_station(request):
     if not request.user.is_superuser:
-        return JsonResponse({'success': False, 'error': 'Permission denied'})
+        return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
 
     received_data = views_helpers.get_received_data(request)
     if not received_data['success']:
@@ -27,14 +27,15 @@ def create_station(request):
     new_station = views_helpers.create_new_station(received_data)
     if not new_station._state.db:
         return JsonResponse({
-            "status": False, "error": "Station does not in database"
+            "status": False, "error": "Станция не была добавлена в базу данных"
         })
 
     new_station_admin = views_helpers.create_new_station_admin(
         received_data, new_station)
     if not new_station_admin._state.db:
         return JsonResponse({
-            "status": False, "error": "Station does not in database"
+            "status": False,
+            "error": "Держатель станции не был добавлен в базу данных"
         })
 
     views_helpers.add_user_model_permissions_to_user(
