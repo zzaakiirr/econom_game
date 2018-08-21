deda685145a575215eb1ca23f96cf6e151a853a3
zzaakiirr <zzaakiirr@gmail.com>
2018-08-21 23:32:53 +0300

    Fixed accounts.tests.test_get_menu_view

--- econom_game/accounts/tests/test_get_menu_view.py
+++ econom_game/accounts/tests/test_get_menu_view.py
@@ -2,12 +2,14 @@ from django.contrib.auth import login
 from django.test import TestCase
 from django.contrib import auth
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.urls import resolve
 import json
 
 from ..views import get_menu
+from accounts.models import User, StationAdmin
+from stations.views_helpers import add_user_model_permissions_to_user
 
 
 class GetMenuTests(TestCase):
@@ -16,65 +18,50 @@ class GetMenuTests(TestCase):
         self.assertEquals(view.func, get_menu)
 
 
-class NotLoggedGetMenuTestCase(TestCase):
+class NotLoggedUserGetMenuTests(TestCase):
     def setUp(self):
         self.url = reverse('get_menu')
-
-
-class LoggedGetMenuTestCase(NotLoggedGetMenuTestCase):
-    def setUp(self):
-        super().setUp()
-        self.user = User.objects.create(username='test', password='test')
-        self.client.force_login(self.user)
-
-
-class NotLoggedUserGetMenuTests(NotLoggedGetMenuTestCase):
-    def setUp(self):
-        super().setUp()
         self.response = self.client.get(self.url)
 
     def test_not_logged_user_get_menu_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
+    def test_not_logged_user_get_menu_return_correct_data(self):
+        expected_data = {"user_allowed_urls": []}
+        response_content = str(self.response.content, encoding='utf8')
+        self.assertJSONEqual(response_content, expected_data)
 
-class LoggedUserHasPermissionToOnePageGetMenuTests(LoggedGetMenuTestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.station_admins = Group.objects.create(name="station_admins")
 
-        content_type = ContentType.objects.get(
-            app_label='accounts', model='group')
-        cls.can_view_station = Permission.objects.create(
-            codename='view_station',
-            name='Can view "/station" page',
-            content_type=content_type
-        )
-
+class LoggedUserGetMenuTests(TestCase):
     def setUp(self):
-        super().setUp()
-        self.station_admins.permissions.add(self.can_view_station)
-        self.user.groups.add(self.station_admins)
-
+        self.user = User.objects.create(email='test', password='test')
+        self.url = reverse('get_menu')
         self.response = self.client.get(self.url)
+        self.client.force_login(self.user)
 
-    def test_logged_user_get_menu_status_code(self):
+    def test_not_logged_user_get_menu_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
-    def test_logged_user_get_menu_return_correct_data(self):
+
+class StationAdminGetMenuTests(LoggedUserGetMenuTests):
+    def setUp(self):
+        super().setUp()
+        add_user_model_permissions_to_user(self.user, StationAdmin)
+        self.response = self.client.get(self.url)
+
+    def test_station_admin_get_menu_return_correct_data(self):
         expected_data = {"user_allowed_urls": ['/admin/station/']}
         response_content = str(self.response.content, encoding='utf8')
         self.assertJSONEqual(response_content, expected_data)
 
 
-class LoggedUserHasNotPermissionToPageGetMenuTests(LoggedGetMenuTestCase):
+class SuperUserGetMenuTests(LoggedUserGetMenuTests):
     def setUp(self):
         super().setUp()
+        add_user_model_permissions_to_user(self.user, User)
         self.response = self.client.get(self.url)
 
-    def test_user_has_not_permission_to_page_get_menu_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
-
-    def test_user_has_no_permission_to_page_get_menu_return_correct_data(self):
-        expected_data = {"user_allowed_urls": []}
+    def test_super_user_get_menu_return_correct_data(self):
+        expected_data = {"user_allowed_urls": ['/admin/add_station/']}
         response_content = str(self.response.content, encoding='utf8')
         self.assertJSONEqual(response_content, expected_data)
