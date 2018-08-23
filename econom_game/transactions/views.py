03056b9b658f9def99e271eb3bc2191eff7a6dc7
zzaakiirr <zzaakiirr@gmail.com>
2018-08-23 23:02:09 +0300

    Added checking if user is superuser in get_banks_list view

--- econom_game/transactions/views.py
+++ econom_game/transactions/views.py
@@ -15,6 +15,9 @@ def make_transaction(request):
 
 
 def get_banks_list(request):
+    if not request.user.is_superuser:
+        return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
+
     banks = Bank.objects.values()
     banks_list = [bank for bank in banks]
     return JsonResponse(banks_list, safe=False)
