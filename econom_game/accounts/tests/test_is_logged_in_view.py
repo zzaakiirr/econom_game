6ad87b1c4af79ec91635e3ce21c0032684a244a8
zzaakiirr <zzaakiirr@gmail.com>
2018-08-23 22:17:07 +0300

    Fixed importing User model + changed expected data dict

--- econom_game/accounts/tests/test_is_logged_in_view.py
+++ econom_game/accounts/tests/test_is_logged_in_view.py
@@ -1,10 +1,9 @@
-import json
 from django.test import TestCase
-from django.contrib import auth
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.urls import resolve
 from django.contrib.auth import authenticate, login
+import json
 
 from ..views import is_logged_in
 
@@ -27,18 +26,18 @@ class IsLoggedInTests(IsLoggedInTestCase):
 class IsLoggedInTrueTests(IsLoggedInTestCase):
     def setUp(self):
         super().setUp()
-        self.user = User.objects.create(username='test', password='test')
-        self.client.force_login(self.user)
+        user = get_user_model().objects.create(email='test', password='test')
+        self.client.force_login(user)
         self.response = self.client.get(self.url)
 
     def test_is_logged_in_true_return_correct_data(self):
-        expected_data = {'username': 'test'}
+        expected_data = {'is_logged_in': True}
         response_content = str(self.response.content, encoding='utf8')
         self.assertJSONEqual(response_content, expected_data)
 
 
 class IsLoggedInFalseTests(IsLoggedInTestCase):
     def test_is_logged_in_false_return_correct_data(self):
-        expected_data = {'username': None}
+        expected_data = {'is_logged_in': False}
         response_content = str(self.response.content, encoding='utf8')
         self.assertJSONEqual(response_content, expected_data)
