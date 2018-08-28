af800aa1a7dc2c82884fd96190d457ef4a147bbe
zzaakiirr <zzaakiirr@gmail.com>
2018-08-28 18:28:27 +0300

    Fixed stations.tests.test_make_bet_view

--- econom_game/stations/tests/test_make_bet_view.py
+++ econom_game/stations/tests/test_make_bet_view.py
@@ -22,7 +22,7 @@ class MakeBetTests(TestCase):
         self.assertEquals(view.func, make_bet)
 
 
-class SuccessfulMakeBetTests(TestCase):
+class SuccessfulMakeBetTestCase(TestCase):
     def setUp(self):
         Bank.objects.create(
             id=1, name='test', deposit=0,
@@ -31,26 +31,33 @@ class SuccessfulMakeBetTests(TestCase):
         self.card = Card.objects.create(
             id=1, card_number='1', chip_number='1', money_amount=100
         )
-        team = Team.objects.create(
+        self.team = Team.objects.create(
             id=1, name='test', owner='test', faculty='test',
             group='test', bank=1, card='1', card_type='card_number'
         )
-        station = Station.objects.create(
+        self.station = Station.objects.create(
             id=1, name='test', owner='test',
             complexity=2.5, min_bet=100, max_bet=200
         )
 
         user = User.objects.create(email='test@test', password='test')
-        StationAdmin.objects.create(user=user, station=station)
+        StationAdmin.objects.create(user=user, station=self.station)
         self.client.force_login(user)
 
         self.url = reverse("make_bet")
-        data = {'card_type': 'card_number', 'card': '1', 'bet_amount': 100}
+        self.data = {
+            'card_type': 'card_number', 'card': '1', 'bet_amount': 100
+        }
+
+
+class SuccessfulMakeBetTests(SuccessfulMakeBetTestCase):
+    def setUp(self):
+        super().setUp()
         self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
+            self.url, json.dumps(self.data), content_type="application/json"
         )
         self.transaction = Transaction.objects.get(
-            sender=team.id, recipient=station.id
+            sender=self.team.id, recipient=self.station.id
         )
 
     def test_success_status_code(self):
@@ -67,10 +74,10 @@ class SuccessfulMakeBetTests(TestCase):
 
 class NotStationAdminMakeBetTests(TestCase):
     def setUp(self):
-        self.url = reverse("make_bet")
+        url = reverse("make_bet")
         data = {'card_type': 'card_number', 'card': '1', 'bet_amount': 100}
         self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
+            url, json.dumps(data), content_type="application/json"
         )
 
     def test_success_status_code(self):
@@ -85,86 +92,7 @@ class NotStationAdminMakeBetTests(TestCase):
         self.assertJSONEqual(response_content, expected_data)
 
 
-class InvalidCardTypeFormatMakeBetTests(NotStationAdminMakeBetTests):
-    def setUp(self):
-        super().setUp()
-        Bank.objects.create(
-            id=1, name='test', deposit=0,
-            credit_for_one_year=0, credit_for_two_years=0
-        )
-        self.card = Card.objects.create(
-            id=1, card_number='1', chip_number='1', money_amount=100
-        )
-        team = Team.objects.create(
-            id=1, name='test', owner='test', faculty='test',
-            group='test', bank=1, card='1', card_type='card_number'
-        )
-        station = Station.objects.create(
-            id=1, name='test', owner='test',
-            complexity=2.5, min_bet=100, max_bet=200
-        )
-
-        user = User.objects.create(email='test@test', password='test')
-        StationAdmin.objects.create(user=user, station=station)
-        self.client.force_login(user)
-
-        data = {'card_type': 'invalid_format', 'card': '1', 'bet_amount': 100}
-        self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
-        )
-
-    def test_return_correct_data(self):
-        expected_data = {
-            'success': False,
-            'error': 'Неверный формат типа карты'
-        }
-        response_content = str(self.response.content, encoding='utf8')
-        self.assertJSONEqual(response_content, expected_data)
-
-
-class InvalidCardNumberFormatMakeBetTests(InvalidCardTypeFormatMakeBetTests):
-    def setUp(self):
-        super().setUp()
-        data = {
-            'card_type': 'card_number',
-            'card': 'invalid_format',
-            'bet_amount': 100
-        }
-        self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
-        )
-
-    def test_return_correct_data(self):
-        expected_data = {
-            'success': False,
-            'error': 'Неверный формат номера карты'
-        }
-        response_content = str(self.response.content, encoding='utf8')
-        self.assertJSONEqual(response_content, expected_data)
-
-
-class InvalidChipNumberFormatMakeBetTests(InvalidCardTypeFormatMakeBetTests):
-    def setUp(self):
-        super().setUp()
-        data = {
-            'card_type': 'chip_number',
-            'card': 'invalid_format',
-            'bet_amount': 100
-        }
-        self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
-        )
-
-    def test_return_correct_data(self):
-        expected_data = {
-            'success': False,
-            'error': 'Неверный формат номера чипа карты'
-        }
-        response_content = str(self.response.content, encoding='utf8')
-        self.assertJSONEqual(response_content, expected_data)
-
-
-class NotGivedOneRequiredFieldMakeBetTests(InvalidCardTypeFormatMakeBetTests):
+class NotGivedOneRequiredFieldMakeBetTests(SuccessfulMakeBetTestCase):
     def setUp(self):
         super().setUp()
         data = {'card': '1', 'bet_amount': 0}
@@ -178,8 +106,7 @@ class NotGivedOneRequiredFieldMakeBetTests(InvalidCardTypeFormatMakeBetTests):
         self.assertJSONEqual(response_content, expected_data)
 
 
-class NotGivedManyRequiredFieldsMakeBetTests(
-        InvalidCardTypeFormatMakeBetTests):
+class NotGivedManyRequiredFieldsMakeBetTests(SuccessfulMakeBetTestCase):
     def setUp(self):
         super().setUp()
         data = {'bet_amount': 0}
@@ -195,76 +122,50 @@ class NotGivedManyRequiredFieldsMakeBetTests(
         self.assertJSONEqual(response_content, expected_data)
 
 
-class CardDoesNotExistTests(InvalidCardTypeFormatMakeBetTests):
-    def setUp(self):
-        station = Station.objects.create(
-            id=1, name='test', owner='test',
-            complexity=2.5, min_bet=100, max_bet=200
-        )
-        user = User.objects.create(email='test@test', password='test')
-        StationAdmin.objects.create(user=user, station=station)
-        self.client.force_login(user)
-
-        url = reverse("make_bet")
-        data = {'card_type': 'card_number', 'card': '2', 'bet_amount': 100}
-        self.response = self.client.post(
-            url, json.dumps(data), content_type="application/json"
-        )
-
-    def test_success_status_code(self):
-        self.assertEquals(self.response.status_code, 200)
-
-    def test_return_correct_data(self):
-        expected_data = {
-            "success": False, "error": "Такой карты не существует"
-        }
-        response_content = str(self.response.content, encoding='utf8')
-        self.assertJSONEqual(response_content, expected_data)
-
-
-class MakeBetWhenNotEnoghMoneyOnTheCardTests(
-        InvalidCardTypeFormatMakeBetTests):
+class MakeInvalidBetForStationTests(SuccessfulMakeBetTests):
     def setUp(self):
         super().setUp()
-        self.card.money_amount = 0
-        self.card.save()
         data = {
-            'card_type': 'chip_number',
+            'card_type': 'card_number',
             'card': '1',
-            'bet_amount': 100
+            'bet_amount': 0
         }
         self.response = self.client.post(
             self.url, json.dumps(data), content_type="application/json"
         )
 
     def test_return_correct_data(self):
-        expected_data = {
-            'success': False,
-            'error': 'Недостаточно средств на карте'
-        }
-        response_content = str(self.response.content, encoding='utf8')
-        self.assertJSONEqual(response_content, expected_data)
-        self.card.delete()
-
-
-class MakeInvalidBetForStationTests(InvalidCardTypeFormatMakeBetTests):
-    def setUp(self):
-        super().setUp()
-
-    def test_make_bet_less_station_min_bet_return_correct_data(self):
-        data = {
-            'card_type': 'chip_number',
-            'card': '1',
-            'bet_amount': 10
-        }
-        self.response = self.client.post(
-            self.url, json.dumps(data), content_type="application/json"
-        )
-
         expected_data = {
             'success': False,
             'error': 'Ставка меньше минимальной или больше максимальной'
         }
         response_content = str(self.response.content, encoding='utf8')
         self.assertJSONEqual(response_content, expected_data)
-        self.card.delete()
+
+
+class MakeBetNotForFirstTimeInStation(SuccessfulMakeBetTestCase):
+    def setUp(self):
+        super().setUp()
+        send_first_time_response = self.client.post(
+            self.url, json.dumps(self.data), content_type="application/json"
+        )
+        self.response = self.client.post(
+            self.url, json.dumps(self.data), content_type="application/json"
+        )
+        self.transaction = Transaction.objects.get(
+            sender=self.team.id, recipient=self.station.id
+        )
+
+    def test_success_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_not_add_transaction_to_database(self):
+        self.assertTrue(self.transaction._state.db)
+
+    def test_return_correct_data(self):
+        expected_data = {
+            'success': False,
+            'error': 'Команда уже проходила станцию'
+        }
+        response_content = str(self.response.content, encoding='utf8')
+        self.assertJSONEqual(response_content, expected_data)
