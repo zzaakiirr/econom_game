58382c03e7f11ae983de98d396e69cb7979b7516
zzaakiirr <zzaakiirr@gmail.com>
2018-08-31 21:57:11 +0300

    Changed all PositiveIntegerFields in all models which can be float to FloatField

--- econom_game/cards/models.py
+++ econom_game/cards/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Card(models.Model):
     card_number = models.CharField(max_length=25, unique=True)
     chip_number = models.CharField(max_length=25, unique=True)
-    money_amount = models.PositiveIntegerField()
+    money_amount = models.FloatField()
 
     def __str__(self):
         return 'card_%d' % self.id
--- econom_game/credits/models.py
+++ econom_game/credits/models.py
@@ -7,7 +7,7 @@ from teams.models import Team
 class Credit(models.Model):
     team = models.ForeignKey(Team, related_name='credit_team', default=None)
     bank = models.ForeignKey(Bank, related_name='credit_bank', default=None)
-    debt_amount = models.PositiveIntegerField(default=0)
+    debt_amount = models.FloatField(default=0)
     term = models.PositiveIntegerField(default=1)
 
     def __str__(self):
