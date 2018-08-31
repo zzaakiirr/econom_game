a56d2aa4892b89e2beba0dc0f326b82e49561468
zzaakiirr <zzaakiirr@gmail.com>
2018-09-01 00:52:39 +0300

    Added checking if game started in transferdepositpercent command

--- econom_game/deposits/management/commands/transferdepositpercent.py
+++ econom_game/deposits/management/commands/transferdepositpercent.py
@@ -2,12 +2,15 @@ from django.core.management.base import BaseCommand, CommandError
 
 from teams.models import Team
 from deposits.models import Deposit
+from timings.models import Timing
 
 
 class Command(BaseCommand):
     help = 'Transfer to teams card percentage of their deposit'
 
     def handle(self, *args, **options):
+        if not Timing.objects.get(id=1).game_started:
+            return
         teams = Team.objects.all()
         for team in teams:
             deposit = Deposit.objects.filter(team=team)[0]
