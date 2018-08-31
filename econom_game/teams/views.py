fdb770bdf6da7074e366289a789af8e545d49b1c
zzaakiirr <zzaakiirr@gmail.com>
2018-08-31 15:34:10 +0300

    Deleted get_all_teams view + Refactored create_team view

--- econom_game/teams/views.py
+++ econom_game/teams/views.py
@@ -1,31 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-from rest_framework import generics
-
-from .models import Team
-
-from .serializers import TeamSerializer
-
-from . import views_helpers
-
-
-class ListTeamsView(generics.ListAPIView):
-    queryset = Team.objects.all()
-    serializer_class = TeamSerializer
+from teams import views_helpers
 
 
 @csrf_exempt
 def create_team(request):
-    received_data = views_helpers.get_received_data(request)
-    if not received_data['success']:
-        return JsonResponse(received_data)
-
-    new_team = views_helpers.create_new_team(received_data)
-    if not new_team._state.db:
-        return JsonResponse({
-            "success": False,
-            "error": "Команда не была добавлена в базу данных"
-        })
-
-    return JsonResponse({"success": True})
+    response = views_helpers.get_create_team_response(request)
+    return JsonResponse(response)
