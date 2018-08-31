6559679edb41caa5ac77e25a36d62fd8bfd48a9d
zzaakiirr <zzaakiirr@gmail.com>
2018-08-31 13:50:42 +0300

    Refactored create_card view the same way like check_card view

--- econom_game/cards/views.py
+++ econom_game/cards/views.py
@@ -1,23 +1,14 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-from cards import create_card_view_helpers
+from cards.create_card_view_helpers import get_create_card_response
 from cards.check_card_view_helpers import get_check_card_response
 
 
 @csrf_exempt
 def create_card(request):
-    received_data = create_card_view_helpers.get_received_data(request)
-    if not received_data['success']:
-        return JsonResponse(received_data)
-
-    new_card = create_card_view_helpers.create_new_card(received_data)
-    if not new_card._state.db:
-        return JsonResponse({
-            "success": False, "error": "Карта не была добавлена в базу данных"
-        })
-
-    return JsonResponse({"success": True})
+    response = get_create_card_response(request)
+    return JsonResponse(response)
 
 
 @csrf_exempt
