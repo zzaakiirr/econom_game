24b9ae6612c552a2b82dce58314d65c53720f2cf
zzaakiirr <zzaakiirr@gmail.com>
2018-09-01 00:14:02 +0300

    Fixed dublicating stationAdmin in create_station view

--- econom_game/stations/signals.py
+++ econom_game/stations/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 from stations.models import Station
 from accounts.models import User, StationAdmin
@@ -7,10 +8,17 @@ from accounts.models import User, StationAdmin
 
 @receiver(post_save, sender=Station)
 def create_station_admin(sender, instance, created, **kwargs):
-    if created:
+    if created and not is_station_admin_already_exist(instance):
         email = 'station_admin_%d' % instance.id
 
         user = User.objects.create(email=email, password=email)
         StationAdmin.objects.create(
             station=instance, user=user
         )
+
+
+def is_station_admin_already_exist(instance):
+    station_admin = StationAdmin.objects.filter(station=instance)
+    if station_admin:
+        return True
+    return False
