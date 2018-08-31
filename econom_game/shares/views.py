6d8fd006873fa95f7554f6be4c69cfb9e36ffc7b
zzaakiirr <zzaakiirr@gmail.com>
2018-08-31 11:56:49 +0300

    Added share_info view

--- econom_game/shares/views.py
+++ econom_game/shares/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from shares import models
 from shares.views_helpers import is_user_financier
+from shares.share_info_helpers import get_share_info
+from cards import check_card_view_helpers as check_card
 
 
 def exchange_rates(request):
@@ -28,7 +30,13 @@ def share_info(request):
     user = request.user
     if not user.is_superuser and not is_user_financier(user):
         return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
-    pass
+
+    received_data = check_card.get_received_data(request)
+    if not received_data['success']:
+        return JsonResponse(received_data)
+
+    share_info = get_share_info(received_data)
+    return JsonResponse(share_info)
 
 
 @csrf_exempt
