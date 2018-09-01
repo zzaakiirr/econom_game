8696434db42b25004af1e9bd0d2a202e7abac583
zzaakiirr <zzaakiirr@gmail.com>
2018-09-01 05:51:01 +0300

    Added give_money & exclude_money views in transaction app

--- econom_game/transactions/views.py
+++ econom_game/transactions/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 
 from . import confirm_transaction_view_helpers
 from cards.check_card_view_helpers import get_received_data
+from shares.get_exchange_rates_helpers import is_user_financier
+from transactions.money_transfer_helpers import get_money_transfer_response
 
 
 @csrf_exempt
@@ -37,3 +39,23 @@ def confirm_transaction(request):
         "success": False,
         "error": "Деньги не были переведены на карту"
     })
+
+
+@csrf_exempt
+def give_money(request):
+    user = request.user
+    if not user.is_superuser and not is_user_financier(user):
+        return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
+
+    response = get_money_transfer_response()
+    return JsonResponse(response)
+
+
+@csrf_exempt
+def exclude_money(request):
+    user = request.user
+    if not user.is_superuser:
+        return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
+
+    response = get_money_transfer_response(give_money=False)
+    return JsonResponse(response)
