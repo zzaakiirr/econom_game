f245b766619fb727408db148e00c819220598cb5
zzaakiirr <zzaakiirr@gmail.com>
2018-09-01 05:55:24 +0300

    Added give_money & exclude_money urls

--- econom_game/transactions/urls.py
+++ econom_game/transactions/urls.py
@@ -9,4 +9,14 @@ urlpatterns = [
         views.confirm_transaction,
         name='confirm_transaction'
     ),
+    url(
+        r'^api/v1/give_money/$',
+        views.give_money,
+        name='give_money'
+    ),
+    url(
+        r'^api/v1/exclude_money/$',
+        views.exclude_money,
+        name='exclude_money'
+    ),
 ]
