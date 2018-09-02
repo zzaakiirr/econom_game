7ddba483a06e704f1b339e2127a8cd181fb1aa9a
zzaakiirr <zzaakiirr@gmail.com>
2018-09-02 18:06:35 +0300

    Added static files (using in React's index.html) + changed not api urls to open index.html

--- econom_game/accounts/operator_allowed_urls.py
+++ econom_game/accounts/operator_allowed_urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 
-from .empty_view import empty_view
+from accounts.views import index_view
 
 
 urlpatterns = [
-    url(r"^admin/deposit/$", empty_view, name='view_deposit'),
-    url(r"^admin/credit/$", empty_view, name='view_credit'),
+    url(r"^admin/deposit/$", index_view, name='view_deposit'),
+    url(r"^admin/credit/$", index_view, name='view_credit'),
 ]
--- econom_game/accounts/super_user_allowed_urls.py
+++ econom_game/accounts/super_user_allowed_urls.py
@@ -1,37 +1,37 @@
 from django.conf.urls import include, url
 
-from .empty_view import empty_view
+from accounts.views import index_view
 
 
 urlpatterns = [
     url(
         r'^admin/add_station/$',
-        empty_view,
+        index_view,
         name='add_station'
     ),
     url(
         r'^admin/add_group/$',
-        empty_view,
+        index_view,
         name='add_group'
     ),
     url(
         r'^admin/shares/$',
-        empty_view,
+        index_view,
         name='view_shares'
     ),
     url(
         r'^admin/give_money/$',
-        empty_view,
+        index_view,
         name='give_money'
     ),
     url(
         r'^admin/confirm_transaction/$',
-        empty_view,
+        index_view,
         name='confirm_transaction'
     ),
     url(
         r'^admin/exclude_money/$',
-        empty_view,
+        index_view,
         name='exclude_money'
     ),
 ]
