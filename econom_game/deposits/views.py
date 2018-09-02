bdf9cd583160fddd2c6c56adf1435e4b9bbbfbd1
zzaakiirr <zzaakiirr@gmail.com>
2018-09-02 22:05:51 +0300

    Refactored deposits.views inves_money view + Refactored its invest_money_helpers

--- econom_game/deposits/views.py
+++ econom_game/deposits/views.py
@@ -7,6 +7,7 @@ from . import invest_money_helpers
 
 from .get_deposit_info_helpers import get_deposit_info_response
 from .exclude_deposit_money_helpers import get_exclude_deposit_money_response
+from .invest_money_helpers import get_invest_money_response
 
 
 @csrf_exempt
@@ -14,23 +15,8 @@ def invest_money(request):
     if not is_user_operator(request.user):
         return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
 
-    received_data = invest_money_helpers.get_received_data(request)
-    if not received_data['success']:
-        return JsonResponse(received_data)
-
-    invest_money_helpers.decrease_team_card_money_amount_to_invest_amount(
-        received_data
-    )
-    deposit = invest_money_helpers.get_increased_team_deposit_or_create_new(
-        received_data
-    )
-    if not deposit._state.db:
-        return JsonResponse({
-            "success": False,
-            "error": "Депозит не был добавлен в базу данных"
-        })
-
-    return JsonResponse({"success": True})
+    response = get_invest_money_response(request)
+    return JsonResponse(response)
 
 
 @csrf_exempt
