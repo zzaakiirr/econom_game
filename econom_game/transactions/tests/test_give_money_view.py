3aa44286a04479ae580260558a7987d374cb05f8
zzaakiirr <zzaakiirr@gmail.com>
2018-09-05 00:01:32 +0300

    Refactored transactions.views give_money view + Added tests for this view

---
+++ econom_game/transactions/tests/test_give_money_view.py
@@ -0,0 +1,115 @@
+from django.urls import reverse, resolve
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+import json
+
+from accounts.models import Financier
+from banks.models import Bank
+from teams.models import Team
+from cards.models import Card
+
+from ..views import give_money
+
+
+User = get_user_model()
+
+
+class GiveMoneyTests(TestCase):
+    def test_give_money_url_resolves_give_money_view(self):
+        view = resolve('/api/v1/give_money/')
+        self.assertEquals(view.func, give_money)
+
+
+class GiveMoneyTestCase(TestCase):
+    def setUp(self):
+        self.bank = Bank.objects.create(
+            name='test', deposit=0,
+            credit_for_one_year=0, credit_for_two_years=0
+        )
+        self.old_card = Card.objects.create(
+            card_number='1', chip_number='1', money_amount=100
+        )
+        self.team = Team.objects.create(
+            name='test', owner='test', faculty='test',
+            group='test', bank=self.bank, card=self.old_card
+        )
+
+        user = User.objects.create(email='test@test', password='test')
+        Financier.objects.create(user=user)
+        self.client.force_login(user)
+
+        self.url = reverse("give_money")
+        self.money_amount = 100
+        self.data = {
+            'card_type': 'card_number', 'card': '1',
+            'money_amount': self.money_amount
+        }
+
+
+class InvalidMoneyAmountFormatGiveMoneyTests(GiveMoneyTestCase):
+    def setUp(self):
+        super().setUp()
+        self.data = {
+            'card_type': 'card_number', 'card': '1',
+            'money_amount': 'invalid_format',
+        }
+        self.response = self.client.post(
+            self.url, json.dumps(self.data), content_type="application/json"
+        )
+        self.changed_card = Card.objects.get(id=self.old_card.id)
+
+    def test_success_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_money_not_transfered_to_team_card(self):
+        self.assertEquals(
+            self.changed_card.money_amount,
+            self.old_card.money_amount
+        )
+
+    def test_return_correct_data(self):
+        expected_data = {
+            'success': False,
+            'error': 'Неверный формат денежной суммы'
+        }
+        self.assertJSONEqual(self.response.content, expected_data)
+
+
+class NotFinancierAndNotSuperUserGiveMoneyTests(
+        InvalidMoneyAmountFormatGiveMoneyTests):
+    def setUp(self):
+        super().setUp()
+        user = User.objects.create(email='test_2@test', password='test')
+        self.client.force_login(user)
+
+        self.data['money_amount'] = 100
+        self.response = self.client.post(
+            self.url, json.dumps(self.data), content_type="application/json"
+        )
+        self.changed_card = Card.objects.get(id=self.old_card.id)
+
+    def test_return_correct_data(self):
+        expected_data = {'success': False, 'error': 'Недостаточно прав'}
+        self.assertJSONEqual(self.response.content, expected_data)
+
+
+class SuccessfulGiveMoneyTests(GiveMoneyTestCase):
+    def setUp(self):
+        super().setUp()
+        self.response = self.client.post(
+            self.url, json.dumps(self.data), content_type="application/json"
+        )
+        self.changed_card = Card.objects.get(id=self.old_card.id)
+
+    def test_success_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_money_transfered_to_team_card(self):
+        self.assertEquals(
+            self.changed_card.money_amount,
+            self.old_card.money_amount + self.money_amount
+        )
+
+    def test_return_correct_data(self):
+        expected_data = {'success': True}
+        self.assertJSONEqual(self.response.content, expected_data)
--- econom_game/transactions/views.py
+++ econom_game/transactions/views.py
@@ -1,9 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
+from shares.views_helpers import is_user_financier
 from .confirm_transaction_helpers import is_user_operator
 from .confirm_transaction_helpers import fetch_confirm_transaction_response
-from transactions.money_transfer_helpers import get_money_transfer_response
+from .transfer_money_helpers import fetch_transfer_money_response
 
 
 @csrf_exempt
@@ -22,7 +23,7 @@ def give_money(request):
     if not user.is_superuser and not is_user_financier(user):
         return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
 
-    response = get_money_transfer_response()
+    response = fetch_transfer_money_response(request, give_money=True)
     return JsonResponse(response)
 
 
@@ -32,4 +33,5 @@ def exclude_money(request):
     if not user.is_superuser:
         return JsonResponse({'success': False, 'error': 'Недостаточно прав'})
 
-    response = get_money_transfer_response(give_money=False)
+    response = fetch_transfer_money_response(request, give_money=False)
+    return JsonResponse(response)
